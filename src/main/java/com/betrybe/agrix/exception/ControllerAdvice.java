c9d405e608ce38827d1f59cbbf8e720f0673f0ce
Mariane Candian <mariane.candian@gmail.com>
2023-08-23 15:12:12 -0300

    fix: Javadoc comment

--- src/main/java/com/betrybe/agrix/exception/ControllerAdvice.java
+++ src/main/java/com/betrybe/agrix/exception/ControllerAdvice.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * ControllerAdvice.
+ */
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
   @ExceptionHandler(NotFoundError.class)
